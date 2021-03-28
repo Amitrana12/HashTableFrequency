25dd0aa2d210a4b8ae9a1f96898db1ea12ed52b3
amit <[amirana14325@gmail.com]>
2021-03-29 02:39:32 +0530

    [Amit Rana]Added::Implement of Hashtable and addding element and aslo check frequency

--- FrequencyHash/FrequencyHash/Program.cs
+++ FrequencyHash/FrequencyHash/Program.cs
@@ -8,7 +8,15 @@ namespace FrequencyHash
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to My HashTable Application BY AMIT RANA");
-
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Operation operation = new Operation();
+            operation.Frequency(hash);
         }
     }
 }
